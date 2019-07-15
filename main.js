9269a979aab6d4ce8661323398bcb13d75c47908
Anthony Michael Stephens <mike@Anthonys-MacBook-Pro.local>
2019-07-15 02:38:53 -0400

    made the loops work for adding number of Li item now attempting to parse string-int from input and concat to li, maybe store in array

--- main.js
+++ main.js
@@ -1,34 +1,42 @@
-const submitButton = document.querySelector('.submit');
+const submitButton = document.querySelector(".submit");
 submitButton.onclick = makeList;
 
 function makeList(event) {
   event.preventDefault();
   deletePreviousList();
 
-  const inputBox = document.querySelector('.user-input');
-  const num = inputBox.value;
-
-  
-
+  const inputBox = document.querySelector(".user-input");
+  const num = parseInt(inputBox.value) + 1;
+  addNewLI(inputBox);
 }
 
 function addNewLI(text) {
-  const ul = document.querySelector('.list');
-  const li = document.createElement('li');
-  li.innerText = text;
-  ul.appendChild(li);
+  let num = parseInt(text.value)+1;
+  for (let i = 1; i < num; i++) {
+  const ul = document.querySelector(".list");
+  const li = document.createElement("li");
+  
+    li.innerText = i + text.value;
+    ul.appendChild(li);
+  }
 }
 
+
+for (let i = 1; i < num; i++) {
+  addNewLI(i);
+  if (i % 2 === 0) {
+    makeLatestLIBold();
+  }
+}
 function deletePreviousList() {
-  const ul = document.querySelector('.list');
+  const ul = document.querySelector(".list");
   while (ul.hasChildNodes()) {
     ul.removeChild(ul.firstChild);
   }
-
 }
 
 function makeLatestLIBold() {
-  const ul = document.querySelector('.list');
+  const ul = document.querySelector(".list");
   const li = ul.lastChild;
-  li.style.fontWeight = 'bold';
+  li.style.fontWeight = "bold";
 }
